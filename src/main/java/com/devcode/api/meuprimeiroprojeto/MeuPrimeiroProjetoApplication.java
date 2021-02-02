a71261103cc08222c904e1c04a08db8a57162ff5
Vitor A. Oliveira <vitoraquino93@hotmail.com>
2021-02-02 20:47:52 -0300

    Alteraçao no arquivo principal da aplicaçao

--- src/main/java/com/devcode/api/meuprimeiroprojeto/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/devcode/api/meuprimeiroprojeto/MeuPrimeiroProjetoApplication.java
@@ -3,11 +3,14 @@ package com.devcode.api.meuprimeiroprojeto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+
+		System.out.println("Meu primeiro projeto.");
 	}
 
 }
