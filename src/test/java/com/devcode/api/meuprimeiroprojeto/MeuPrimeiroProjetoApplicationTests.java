253780936140dcec3091085dc6783c25a5d9172b
Vitor A. Oliveira <vitoraquino93@hotmail.com>
2021-02-02 20:34:11 -0300

    Versao Inicial do Projeto.

---
+++ src/test/java/com/devcode/api/meuprimeiroprojeto/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcode.api.meuprimeiroprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
